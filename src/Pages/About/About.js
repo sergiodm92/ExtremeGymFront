ac178a6e8bab9a66749992c77b3811da0b6fc566
MariRionda <maririonda@gmail.com>
2023-03-17 20:17:49 -0300

    sobre nosotros - primera parte

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -6,7 +6,11 @@ const About = ()=>{
     return(
         <div className={style.conteiner}>
         <Navbar/>
-            <h1>About</h1>
+        <div className={style.title}>
+            <h1 className={style.bigOne}>Sobre nosotro</h1>
+            <p className={style.coment}>Extreme Gym nace en la provincia de Salta - Argentina en julio del año 2008.</p>
+            <h1 className={style.bigOne}>Ubicación</h1>
+        </div>
         </div>
     )
 }
